9fbffeeb08a6508d35684d80bdd742157b52b201
affogatoman <colombia2@naver.com>
2017-01-07 18:08:44 +0900

    CONFLICT

--- app/src/main/java/me/blog/colombia2/schoolparser/parser/SchoolFoodParser.java
+++ app/src/main/java/me/blog/colombia2/schoolparser/parser/SchoolFoodParser.java
@@ -9,7 +9,6 @@ public class SchoolFoodParser {
     public static String getTable(int type) throws IOException {
         String result = "";
         
-<<<<<<< HEAD
         School api = new School(School.Type.HIGH, School.Region.CHUNGBUK, "M100001915");
 		try {
 			ArrayList<SchoolMenu> list = new ArrayList<>(api.getMonthlyMenu(DateInstance.YEAR, DateInstance.MONTH));
@@ -29,30 +28,5 @@ public class SchoolFoodParser {
 		}
 		
         return result.replaceAll("[0-9\\.]", "").replace("급식이 없습니다", "급식 정보가 없습니다");
-=======
-        String schYmd = DateInstance.YEAR+"."+getMonthFormat(DateInstance.MONTH)+"."+DateInstance.DATE;
-        int currentDay = DateInstance.DAY;
-        Document doc = Jsoup.connect("http://stu.cbe.go.kr/edusys.jsp?page=sts_m42310")
-                            .timeout(10*1000)
-                            .data("schulCode", "M100001915")
-                            .data("schulCrseScCode", "4")
-                            .data("schulKndScCode", "04")
-                            .data("schYmd", schYmd)
-                            .data("schMmealScCode", type+"")
-                            .get();
-        Elements foodList = doc.select("tbody tr").get(1).select("td");
-        result = (Html.fromHtml(foodList.get(currentDay).toString())+"").replaceAll("[①-⑬0-9]", "");
-        if(result.equals(""))
-            result = "급식 정보가 없습니다.";
-        
-        return result; 
-    }
-    
-    private static String getMonthFormat(int month) {
-        String m = String.valueOf(month);
-        if(m.length() == 1)
-            m = "0"+m;
-        return m;
->>>>>>> branch 'master' of https://github.com/affogatoman/SchoolParser.git
     }
 }
